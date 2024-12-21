e5f0af8dbc22e2a9fadc39daad4d1a875feace26
S.Ageev <Ageev.sa27@yandex.ru>
2024-12-22 01:07:22 +0300

    new version

--- greet.go
+++ greet.go
@@ -12,3 +12,11 @@ func GetGreet(name string) (string, error) {
 		return fmt.Sprintf("Привет, %s, как жизнь ??", name), nil
 	}
 }
+
+func Hi(name string) {
+	if name == "" {
+		panic(errors.New("нет имени"))
+	} else {
+		fmt.Printf("привет %s", name)
+	}
+}
